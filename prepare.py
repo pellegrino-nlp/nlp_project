c11869dffa4ba5c7fbb50f831da6f70c39c11611
faithkane3 <faith.x.kane@gmail.com>
2019-12-10 21:00:38 -0600

    Commit updated funcs in file including names

--- prepare.py
+++ prepare.py
@@ -10,17 +10,68 @@ import pandas as pd
 
 import acquire
 
-def basic_clean(string):
+def normalize(string):
     """
-    Lowercase the string
-    Normalize unicode characters
-    Replace anything that is not a letter, number, whitespace or a single quote.
+    Convert to all lowercase  
+    Normalize the unicode chars  
+    Remove any non-alpha or whitespace characters  
+    Remove any alpha strings with 2 characters or less  
     """
     string = string.lower()
     string = unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-    # remove anything not a space character, an apostrophy, letter, or number
-    string = re.sub(r"[^a-z0-9'\s]", '', string)
-    # convert newlins and tabs to a single space
+    
+    # keep only alpha chars
+    string = re.sub(r'[^a-z]', ' ', string)
+    
+    # remove strings less than 2 chars in length
+    string = re.sub(r'\b[a-z]{,2}\b', '', string)
+    
+    # convert newlines and tabs to a single space
     string = re.sub(r'[\r|\n|\r\n]+', ' ', string)
+    
+    # strip extra whitespace
     string = string.strip()
+    
     return string
+
+
+def stem(string):
+    ps = nltk.porter.PorterStemmer()
+    stems = [ps.stem(word) for word in string.split()]
+    string_of_stems = ' '.join(stems)
+    return string_of_stems
+
+def lemmatize(string):
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemmas = [wnl.lemmatize(word) for word in string.split()]
+    string_of_lemmas = ' '.join(lemmas)
+    return string_of_lemmas
+
+def tokenize(string):
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    return tokenizer.tokenize(string, return_str=True)
+
+extra_words = ['http', 'https', 'www', 'banner', 'request', 'img', 'com', 'png', 'welcome']
+
+def remove_stopwords(tokenized_string, extra_words=extra_words, exclude_words=[]):
+    words = tokenized_string.split()
+    stopword_list = stopwords.words('english')
+
+    # remove the excluded words from the stopword list
+    stopword_list = set(stopword_list) - set(exclude_words)
+
+    # add in the user specified extra words
+    stopword_list = stopword_list.union(set(extra_words))
+
+    filtered_words = [w for w in words if w not in stopword_list]
+    final_string = " ".join(filtered_words)
+    return final_string
+
+def prep_articles(df):
+    df = df.assign(original = df.readme_contents)
+    df = df.assign(normalized = df.original.apply(normalize))
+    df = df.assign(stemmed = df.normalized.apply(stem))
+    df = df.assign(lemmatized = df.normalized.apply(lemmatize))
+    df = df.assign(cleaned = df.lemmatized.apply(remove_stopwords))
+    df.drop(columns=(["readme_contents", "repo"]), inplace=True)
+    return df
